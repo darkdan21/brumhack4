069d05b4e45c43792fc1ee6ce3d1698c1a5593be
Dan Jones <dan_j@live.co.uk>
2016-03-12 15:54:31 +0000

    Does alpha work?

--- controller/script.js
+++ controller/script.js
@@ -1,10 +1,10 @@
 $(document).ready(function () {
 
     function handleOrientation(event) {
+        console.log(event);
         var alpha = event.alpha;
 
-        $('#alpha').text("Alpha : ");
-        $('#alpha').append(alpha);
+        $('#alpha').text("Alpha: " + alpha);
     }
 
     $(window).bind('deviceorientation', handleOrientation);
