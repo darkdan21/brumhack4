0e1c90ee18cda611b64eb5badc6f6a8084c2cf1b
Joseph Groocock <frebib@gmail.com>
2016-03-12 20:01:11 +0000

    Add WebSocketServer

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
 var express = require('express'),
-    http = require('http');
+    http = require('http'),
+    ws = require('ws');
 
 var app = express();
 var server = http.createServer(app).listen(8080);
+
+var wss = new ws.Server({
+    server: server,
+    path: '/control'
+});
 
 // Serve static content
 app.use(express.static('public'));
