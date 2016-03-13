5ba96e8f8b3e4be146c527a4884d0ffd57f21073
Joseph Groocock <frebib@gmail.com>
2016-03-13 03:54:42 +0000

    Cleaned up controller js

--- public/javascript/controller.js
+++ public/javascript/controller.js
@@ -1,43 +1,53 @@
 $(document).ready(function () {
-
     var ws = new WebSocket("wss://drunkdrivingsimulator.com/control");
     var rotation = 0;
     var rotationnew;
 
-    function devicemotion(e) {
-        rotationnew = e.originalEvent.accelerationIncludingGravity.y;
-    }
+    $(window).bind('devicemotion', function(e) {
+        rotation = e.originalEvent.accelerationIncludingGravity.y;
+    });
 
-    $(window).bind('devicemotion', devicemotion);
+    ws.on('open', function () {
+        setInterval(function() {
+            if (ws.readyState !== 1) return;
 
-
-    window.setInterval(function () {
-        if (Math.abs(rotation - rotationnew) > 1) {
-            rotation = rotationnew;
-            if(rotation > 15) {
-                rotation = 15;
+            if (Math.abs(rotation - rotationnew) > 1) {
+                rotation = rotationnew;
+                if (rotation > 15) {
+                    rotation = 15;
+                }
+                if (rotation < -15) {
+                    rotation = -15;
+                }
             }
-            if(rotation < -15){
-                rotation =- 15;
-            }
-            ws.send("1:" + rotationnew);
-            console.log("Am I running?");
+            send('rot', rotation);
+        }, 100);
+    });
+
+    ws.on('message', function(msg) {
+        console.log('got: ' + msg);
+    });
+
+    $('#brake').on('touchstart mousedown', function (e) {
+        send('brk', 1);
+    });
+    $('#brake').on('touchend mouseup', function (e) {
+        send('brk', 0);
+    });
+
+    $('#accelerate').on('touchstart mousedown', function (e) {
+        send('acc', 1);
+    });
+    $('#accelerate').on('touchend mouseup', function (e) {
+        send('acc', 0);
+    });
+
+    function send(event, data) {
+        if (ws.readyState === 1) {
+            ws.send({
+                type: event,
+                data: data
+            });
         }
-    }, 100);
-
-    $('#accelerate').bind('touchstart',function () {
-        ws.send("2:1")
-    });
-
-    $('#brake').bind('touchstart',function () {
-        ws.send("3:1")
-    });
-
-    $('#accelerate').bind('touchend',function () {
-        ws.send("2:0")
-    });
-
-    $('#brake').bind('touchend',function () {
-        ws.send("3:0")
-    });
+    }
 });
