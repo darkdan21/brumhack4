8c66f1748c7ece04b31360574bd9d49711a8443a
Joseph Groocock <frebib@gmail.com>
2016-03-13 03:55:08 +0000

    Server: Handle some game IDs

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     http = require('http'),
-    ws = require('ws');
+    ws = require('ws'),
+    cookies = require('cookie-parser');
 
 var app = express();
 var server = http.createServer(app).listen(8080);
@@ -8,16 +9,39 @@ var root = { root : __dirname };
 
 // Serve static content
 app.use(express.static('public'));
+app.use(cookies());
 
-// Handle Viewer
-app.get('/viewer/:id', function(req, res){
+var idmap = [];
+
+app.get('/play', function(req, res){
+    var mkid = function() { // Gets a random 8 char hex string
+        return Math.floor((1 + Math.random()) * 0x100000000)
+            .toString(16).substring(1);
+    };
+    var id = mkid();
+    var secret = mkid() + mkid();
+    var hash = hashCode(id + secret);
+
+    res.cookie('gameid', hash);
+    res.cookie('hash', hash);
+    idmap[id] = secret;
+
+    res.redirect("/controller/" + id);
+});
+
+// Handtle Viewer
+app.get('/viewer', function(req, res){
 });
 
 // Handle controller
-app.get('/controller/:id', function(req, res){
+app.get('/controller', function(req, res){
+    var id = req.cookies.gameid;
+    var hash = req.cookies.hash;
+    if (!id || !hash)
+        return noGame(req, res);
+
     res.sendFile('html/controller.html', root);
 });
-
 
 // Handle controller input
 var wss = new ws.Server({
@@ -25,7 +49,22 @@ var wss = new ws.Server({
     path: '/control'
 });
 wss.on('connection', function connection(ws) {
-    ws.on('message', function incoming(message) {
-        console.log('received: %s', message);
+    ws.on('message', function(msg) {
+        console.log('received: %s', msg);
     });
 });
+
+function noGame(req, res) {
+    return req.end('No active game with id ' + req.cookies.gameid);
+}
+
+function hashCode(str) {
+    var hash = 0, i, chr, len;
+    if (str.length === 0) return hash;
+    for (i = 0, len = str.length; i < len; i++) {
+        chr   = str.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+}
